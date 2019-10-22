e5f2e5bc58beb8ebb4ceb6962f4da6cd549b7a55
Foster Gailey <foster@gaileyhome.com>
2019-10-22 14:58:24 -0400

    added content area

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
-$('#navId a').click(e => {
+// *******************NAV BAR************************
+//cant use arrow function because of this
+//changes tab bg colors
+$('.selector').click(function(e) {
     e.preventDefault();
-    $(this).tab('show');
-});
+    console.log(this)
+    $('.selector').removeClass('bg-secondary text-white')
+    $(this).addClass('bg-secondary text-white')
+    
+})
+// *******************End Nav Bar*********************
+
+// *******************CONTENT AREA********************
+//hides WELCOME on home screen on click
+$(document).click( (e) =>{
+    e.preventDefault();
+    $('.middleScreen').hide();
+})
+
+
+// *******************END CONTENT*********************
